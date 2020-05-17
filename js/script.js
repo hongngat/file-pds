bcdade398cd3946e548af719bd30b81c25fb08c9
hongngat <48482193+hongngat@users.noreply.github.com>
2020-05-17 22:56:03 +0700

    a

--- js/script.js
+++ js/script.js
@@ -16,3 +16,20 @@ jQuery(document).ready(function () {
             });
     });
 });
+$(window).load(function(){
+    $("#fade-example .js-tabs-link").aniTabs();
+    $("#slide-example .js-tabs-link").aniTabs({animation:"slide", animationSpeed:600, dragSpeed:600});
+    $("#custom-example .js-tabs-link").aniTabs({animation:"slide", animationSpeed:800, slideDirection:"right", drag:false, autoHeight:false});
+    $("#multiple-example .js-tabs-link").aniTabs();
+    $("#multiple-inner .js-tabs-link").aniTabs({animation:"slide"});
+})
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
